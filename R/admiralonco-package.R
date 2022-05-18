d29946f56f1ba49126b22fed90012e96fc3ebef8
Bundfuss, Stefan {MDBB~Basel} <stefan.bundfuss@roche.com>
2022-05-18 12:21:48 +0200

    30_filter_confirmation: implement filter_confirmation()

--- R/admiralonco-package.R
+++ R/admiralonco-package.R
@@ -1,3 +1,7 @@
 #' @keywords internal
-#' @importFrom dplyr if_else mutate select
+#' @importFrom admiral assert_character_scalar assert_data_frame
+#'   assert_filter_cond assert_order_vars assert_vars derive_var_obs_number
+#'   expect_dfs_equal filter_relative vars2chr
+#' @importFrom dplyr filter group_by if_else left_join mutate select slice ungroup
+#' @importFrom rlang enquo quo_is_null
 "_PACKAGE"
