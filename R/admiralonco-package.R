1c7cd284823bac6210d5f5dde7b83af6dbd2096f
Bundfuss, Stefan {MDBB~Basel} <stefan.bundfuss@roche.com>
2022-05-25 14:25:01 +0200

    30_filter_confirmation: fix R-CMD check

--- R/admiralonco-package.R
+++ R/admiralonco-package.R
@@ -3,6 +3,7 @@
 #'   assert_filter_cond assert_order_vars assert_vars derive_var_obs_number
 #'   expect_dfs_equal filter_relative vars2chr
 #' @importFrom dplyr filter group_by if_else left_join mutate select slice
-#'   ungroup
+#'   vars ungroup
+#' @importFrom magrittr `%>%`
 #' @importFrom rlang enquo quo_is_null
 "_PACKAGE"
