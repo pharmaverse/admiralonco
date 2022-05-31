8439c1d49426c67a3352c37a5213dba87f325cd9
Bundfuss, Stefan {MDBB~Basel} <stefan.bundfuss@roche.com>
2022-05-31 12:08:11 +0200

    23_derive_param_confrimed_response.R

---
+++ R/derive_param_confirmed_response.R
@@ -0,0 +1,383 @@
+#' Derive Confirmed Response Parameter
+#'
+#' Derive confirmed response parameter
+#'
+#' @param dataset Input dataset
+#'
+#'   The `PARAMCD`, `ADT`, and `AVALC` variables and the variables specified by
+#'   `subject_keys` and `reference_date` are expected.
+#'
+#' @param dataset_adsl ADSL input dataset
+#'
+#'   The variables specified for `subject_keys` are expected. For each subject
+#'   of the specified dataset a new observation is added to the input dataset.
+#'
+#' @param filter_source Source filter
+#'
+#'   All observations in `dataset_source` fulfilling the specified condition are
+#'   considered for deriving the confirmed best overall response.
+#'
+#' @param source_pd Date of first progressive disease (PD)
+#'
+#'   If the parameter is specified, the observations of the input dataset for
+#'   deriving the new parameter are restricted to observations up to the
+#'   specified date. Observations at the specified date are included. For
+#'   subjects without first PD date all observations are take into account.
+#'
+#'   *Permitted Values:* a `date_source` object (see `admiral::date_source()`
+#'   for details)
+#'
+#'   *Default:* `NULL`,
+#'
+#' @param source_datasets Source dataset for the first PD date
+#'
+#'   A named list of datasets is expected. It links the `dataset_name` from
+#'   `source_pd` with an existing dataset.
+#'
+#'   For example if `source_pd = pd_date` with
+#'   ```
+#'   pd_date <- date_source(
+#'     dataset_name = "adrs",
+#'     date = ADT,
+#'     filter = PARAMCD == PD
+#'   )
+#'   ```
+#'   and the actual response dataset in the script is `myadrs`, `source_datasets
+#'   = list(adrs = myadrs)` should be specified.
+#'
+#'   This allows to define `pd_date` at a higher level, e.g., at company level,
+#'   where the actual dataset does not exist.
+#'
+#' @param reference_date Reference date
+#'
+#'   The reference date is used for the derivation of `"SD"` and
+#'   `"NON-CR/NON-PD"` response (see "Details" section). Usually it is treatment
+#'   start date (`TRTSDT`) or randomization date (`RANDDT`).
+#'
+#'   *Permitted Values:* a numeric date variable
+#'
+#' @param ref_start_window Stable disease time window
+#'
+#'   Assessments at least the specified number of days after the reference date
+#'   with response `"CR"`, `"PR"`, `"SD"`, or `"NON-CR/NON-PD"` are considered
+#'   as `"SD"` or `"NON-CR/NON-PD"` response.
+#'
+#'   *Permitted Values:* a non-negative numeric scalar
+#'
+#' @param ref_confirm Minimum time period for confirmation
+#'
+#'   The assessment and the confirmatory assessment for `"CR"` and `"PR"` have
+#'   to be at least the specified number of days apart.
+#'
+#' @param max_nr_ne
+#'
+#'   The specified number of `"NE"` assessments between the assessment and the
+#'   confirmatory assessment for `"CR"` and `"PR"` response is accepted.
+#'
+#'   *Permitted Values:* a non-negative numeric scalar
+#'
+#'   *Default:* `1`
+#' @param accept_sd Accept `"SD"` for `"PR"`?
+#'
+#'   If the argument is set to `TRUE`, one `"SD"` assessment between the
+#'   assessment and the confirmatory assessment for `"PR"` response is accepted.
+#'   Otherwise, no `"SD"` assessment must occur between the two assessments.
+#'
+#'   *Permitted Values:* a logical scalar
+#'
+#'   *Default:* `FALSE`
+#'
+#' @param missing_as_ne Consider no assessments as `"NE"`?
+#'
+#'   If the argument is set to `TRUE`, the response is set to `"NE"` for
+#'   subjects without an assessment in the input dataset. Otherwise, the
+#'   response is set to `"MISSING"` for these subjects.
+#'
+#'   *Permitted Values:* a logical scalar
+#'
+#'   *Default:* `FALSE`
+#'
+#' @param aval_fun Function to map character analysis value (`AVALC`) to numeric
+#'   analysis value (`AVAL`)
+#'
+#'   The (first) argument of the function must expect a character vector and the
+#'   function must return a numeric vector.
+#'
+#'   *Default:* `aval_resp` (see `aval_resp()` for details)
+#'
+#' @param set_values_to Variables to set
+#'
+#'   A named list returned by `vars()` defining the variables to be set for the
+#'   new parameter, e.g. `vars(PARAMCD = "CBOR", PARAM = "Confirmed Best Overall
+#'   Response")` is expected. The values must be symbols, character strings,
+#'   numeric values, or `NA`.
+#'
+#' @param subject_keys Variables to uniquely identify a subject
+#'
+#'   A list of symbols created using `vars()` is expected.
+#'
+#' @details
+#'
+#'   1. The input dataset (`dataset`) is restricted to the observations matching
+#'   `filter_source` and to observations before or at the date specified by
+#'   `source_pd`.
+#'
+#'   1. A subject is considered as responder if there is at least one observation in the restricted dataset with
+#'
+#'       - `AVALC == "CR"`,
+#'       - there is a confirmatory assessment with `AVALC == "CR"` at least
+#'       `ref_confirm` days after the assessment,
+#'       - all assessments between the assessment and the confirmatory
+#'       assessment are `"CR"` or `"NE"`, and
+#'       - there are at most `max_nr_ne` `"NE"` assessments between the
+#'       assessment and the confirmatory assessment.
+#'
+#'     or at least one observation with
+#'           - `AVALC == "PR"`,
+#'           - there is a confirmatory assessment with `AVALC %in% c("CR",
+#'           "PR")` at least `ref_confirm` days after the assessment,
+#'           - all assessments between the assessment and the confirmatory
+#'           assessment are `"CR"`, `"PR"`, or `"NE"`,
+#'           - there is no `"PR"` assessment after a `"CR"` assessment in the
+#'           confirmation period, and
+#'           - there are at most `max_nr_ne` `"NE"` assessments between the
+#'           assessment and the confirmatory assessment.
+#'
+#'           If the `accept_sd` argument is set to `TRUE`, one `"SD"` assessment
+#'           in the confirmation period is accepted.
+#'
+#'   1. For responders `AVALC` is set to `"Y"` and `ADT` to the first date where
+#'   the response criteria are fulfilled. For all other subjects in
+#'   `dataset_adsl` `AVALC` is set to `"N"` and `ADT` to `NA`.
+#'
+#'   1. The `AVAL` variable is added and set to `aval_fun(AVALC)`.
+#'
+#'   1. The variables specified by the `set_values_to` parameter are added to
+#'   the new observations.
+#'
+#'   1. The new observations are added to input dataset.
+#'
+#' @return The input dataset with a new parameter for confirmed response
+#'
+#' @keywords derivation adrs
+#'
+#' @author Stefan Bundfuss
+#'
+#' @export
+#'
+#' @examples
+#'
+#' # Create ADSL dataset
+#' adsl <- tibble::tribble(
+#'   ~USUBJID, ~TRTSDTC,
+#'   "1",      "2020-01-01",
+#'   "2",      "2019-12-12",
+#'   "3",      "2019-11-11",
+#'   "4",      "2019-12-30",
+#'   "5",      "2020-01-01",
+#'   "6",      "2020-02-02",
+#'   "7",      "2020-02-02",
+#'   "8",      "2020-04-01"
+#' ) %>%
+#'   mutate(
+#'     TRTSDT = lubridate::ymd(TRTSDTC),
+#'     STUDYID = "XX1234"
+#'   )
+#'
+#' # Create ADRS dataset
+#' ovr_obs <- tibble::tribble(
+#'   ~USUBJID, ~ADTC,        ~AVALC,
+#'   "1",      "2020-01-01", "PR",
+#'   "1",      "2020-02-01", "CR",
+#'   "1",      "2020-02-16", "NE",
+#'   "1",      "2020-03-01", "CR",
+#'   "1",      "2020-04-01", "SD",
+#'   "2",      "2020-01-01", "SD",
+#'   "2",      "2020-02-01", "PR",
+#'   "2",      "2020-03-01", "SD",
+#'   "2",      "2020-03-13", "CR",
+#'   "3",      "2019-11-12", "CR",
+#'   "3",      "2019-12-02", "CR",
+#'   "3",      "2020-01-01", "SD",
+#'   "4",      "2020-01-01", "PR",
+#'   "4",      "2020-03-01", "SD",
+#'   "4",      "2020-04-01", "SD",
+#'   "4",      "2020-05-01", "PR",
+#'   "4",      "2020-05-15", "NON-CR/NON-PD",
+#'   "5",      "2020-01-01", "PR",
+#'   "5",      "2020-01-10", "SD",
+#'   "5",      "2020-01-20", "PR",
+#'   "5",      "2020-05-15", "NON-CR/NON-PD",
+#'   "6",      "2020-02-06", "PR",
+#'   "6",      "2020-02-16", "CR",
+#'   "6",      "2020-03-30", "PR",
+#'   "6",      "2020-04-12", "PD",
+#'   "6",      "2020-05-01", "CR",
+#'   "6",      "2020-06-01", "CR",
+#'   "7",      "2020-02-06", "PR",
+#'   "7",      "2020-02-16", "CR",
+#'   "7",      "2020-04-01", "NE"
+#' ) %>%
+#'   mutate(PARAMCD = "OVR")
+#'
+#' pd_obs <-
+#'   bind_rows(tibble::tribble(
+#'     ~USUBJID, ~ADTC,        ~AVALC,
+#'     "2",      "2020-03-01", "Y",
+#'     "4",      "2020-02-01", "Y"
+#'   ) %>%
+#'     mutate(PARAMCD = "PD"))
+#' adrs <- bind_rows(ovr_obs, pd_obs) %>%
+#'   mutate(
+#'     ADT = lubridate::ymd(ADTC),
+#'     STUDYID = "XX1234"
+#'   ) %>%
+#'   select(-ADTC) %>%
+#'   admiral::derive_vars_merged(
+#'     dataset_add = adsl,
+#'     by_vars = dplyr::vars(STUDYID, USUBJID),
+#'     new_vars = dplyr::vars(TRTSDT)
+#'   )
+#'
+#' pd_date <- admiral::date_source(
+#'   dataset_name = "adrs",
+#'   date = ADT,
+#'   filter = PARAMCD == "PD"
+#' )
+#'
+#' # Derive confirmed best overall response parameter
+#' derive_param_confirmed_response(
+#'   adrs,
+#'   dataset_adsl = adsl,
+#'   filter_source = PARAMCD == "OVR",
+#'   source_pd = pd_date,
+#'   source_datasets = list(adrs = adrs),
+#'   ref_confirm = 28,
+#'   set_values_to = vars(
+#'     PARAMCD = "CRSP",
+#'     PARAM = "Confirmed Response by Investigator"
+#'   )
+#' ) %>%
+#'   filter(PARAMCD == "CBOR")
+#'
+#' # Derive confirmed best overall response parameter (accepting SD for PR)
+#' derive_param_confirmed_response(
+#'   adrs,
+#'   dataset_adsl = adsl,
+#'   filter_source = PARAMCD == "OVR",
+#'   source_pd = pd_date,
+#'   source_datasets = list(adrs = adrs),
+#'   ref_confirm = 28,
+#'   accept_sd = TRUE,
+#'   set_values_to = vars(
+#'     PARAMCD = "CRSP",
+#'     PARAM = "Confirmed Response by Investigator"
+#'   )
+#' ) %>%
+#'   filter(PARAMCD == "CRSP")
+derive_param_confirmed_response <- function(dataset,
+                                       dataset_adsl,
+                                       filter_source,
+                                       source_pd = NULL,
+                                       source_datasets,
+                                       ref_confirm,
+                                       max_nr_ne = 1,
+                                       accept_sd = FALSE,
+                                       aval_fun = yn_to_numeric,
+                                       set_values_to,
+                                       subject_keys = vars(STUDYID, USUBJID)) {
+  # Check input parameters
+  filter_source <- assert_filter_cond(enquo(filter_source))
+  assert_s3_class(source_pd, "date_source", optional = TRUE)
+  assert_list_of(source_datasets, "data.frame")
+  assert_integer_scalar(ref_confirm, subset = "non-negative")
+  assert_integer_scalar(max_nr_ne, subset = "non-negative")
+  assert_logical_scalar(accept_sd)
+  assert_function(aval_fun)
+  assert_varval_list(set_values_to, required_elements = "PARAMCD")
+  assert_vars(subject_keys)
+  assert_data_frame(
+    dataset,
+    required_vars = admiral:::quo_c(subject_keys, reference_date, vars(PARAMCD, ADT, AVALC))
+  )
+  assert_data_frame(dataset_adsl, required_vars = subject_keys)
+  if (!is.null(dataset)) {
+    assert_param_does_not_exist(dataset, quo_get_expr(set_values_to$PARAMCD))
+  }
+
+  # Restrict input dataset
+  source_data <- dataset %>%
+    filter(!!filter_source)
+  # filter_pd(source_pd = source_pd,
+  #    source_datasets = source_datasets)
+
+  # Create observations for potential responses
+  cr_data <- filter_confirmation(
+    source_data,
+    by_vars = subject_keys,
+    join_vars = vars(AVALC, ADT),
+    order = vars(ADT),
+    first_cond = AVALC.join == "CR" &
+      ADT.join >= ADT + days(ref_confirm),
+    filter = AVALC == "CR" &
+      all(AVALC.join %in% c("CR", "NE")) &
+      count_vals(var = AVALC.join, val = "NE") <= max_nr_ne
+  ) %>%
+    mutate(
+      AVALC = "Y",
+      tmp_order = 1) %>%
+    select(!!!subject_keys, AVALC, tmp_order, ADT)
+
+  if (accept_sd) {
+    max_nr_sd <- 1
+  } else {
+    max_nr_sd <- 0
+  }
+  pr_data <- filter_confirmation(
+    source_data,
+    by_vars = subject_keys,
+    join_vars = vars(AVALC, ADT),
+    order = vars(ADT),
+    first_cond = AVALC.join %in% c("CR", "PR") &
+      ADT.join >= ADT + days(ref_confirm),
+    filter = AVALC == "PR" &
+      all(AVALC.join %in% c("CR", "PR", "SD", "NE")) &
+      count_vals(var = AVALC.join, val = "NE") <= max_nr_ne &
+      count_vals(var = AVALC.join, val = "SD") <= max_nr_sd &
+      (
+        min_cond(
+          var = ADT.join,
+          cond = AVALC.join == "CR"
+        ) > max_cond(var = ADT.join, cond = AVALC.join == "PR") |
+          count_vals(var = AVALC.join, val = "CR") == 0
+      )
+  ) %>%
+    mutate(
+      AVALC = "Y",
+      tmp_order = 1) %>%
+    select(!!!subject_keys, AVALC, tmp_order, ADT)
+
+  missing_data <- dataset_adsl %>%
+    select(!!!subject_keys) %>%
+    mutate(
+      AVALC = "N",
+      tmp_order = 2
+    ) %>%
+    select(!!!subject_keys, AVALC, tmp_order)
+
+  # Select response
+  rsp <- bind_rows(cr_data, pr_data, missing_data) %>%
+    filter_extreme(
+      by_vars = subject_keys,
+      order = vars(tmp_order, ADT),
+      mode = "first"
+    ) %>%
+    select(-tmp_order) %>%
+    mutate(
+      AVAL = aval_fun(AVALC),
+      !!!set_values_to
+    )
+
+  # Add to input dataset
+  bind_rows(dataset, rsp)
+}
