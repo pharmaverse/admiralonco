2b6e1677a9af69f31deb39c718314f6f34d1b9e3
Bundfuss, Stefan {MDBB~Basel} <stefan.bundfuss@roche.com>
2022-06-03 14:09:37 +0200

    18_filter_pd: move and update unit tests

---
+++ tests/testthat/test-filter_pd.R
@@ -0,0 +1,154 @@
+# filter_pd ----
+## filter_pd Test 1: first PD in separate dataset ----
+test_that("filter_pd Test 1: first PD in separate dataset", {
+  adrs <- tibble::tribble(
+    ~STUDYID,       ~USUBJID,      ~PARAMCD, ~AVALC, ~ADT,
+    "CDISCPILOT01", "01-701-1015", "OVR",    "CR",   "2016-01-25",
+    "CDISCPILOT01", "01-701-1015", "OVR",    "PD",   "2016-02-22",
+    "CDISCPILOT01", "01-701-1015", "BOR",    "CR",   "2016-01-25",
+    "CDISCPILOT01", "01-701-1034", "OVR",    "SD",   "2015-12-07",
+    "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-04-25",
+    "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-06-25",
+    "CDISCPILOT01", "01-701-1034", "BOR",    "SD",   "2015-12-07",
+    "CDISCPILOT01", "01-701-1035", "OVR",    "SD",   "2016-04-25",
+    "CDISCPILOT01", "01-701-1035", "OVR",    "PR",   "2016-06-25",
+    "CDISCPILOT01", "01-701-1035", "BOR",    "PR",   "2016-06-25"
+  ) %>% dplyr::mutate(
+    ADT = lubridate::as_date(ADT)
+  )
+
+  adevent <- tibble::tribble(
+    ~STUDYID,       ~USUBJID,      ~PARAMCD, ~AVALC, ~ADT,
+    "CDISCPILOT01", "01-701-1015", "PD",     "Y",    "2016-02-22",
+    "CDISCPILOT01", "01-701-1034", "PD",     "Y",    "2016-04-25"
+  ) %>% dplyr::mutate(
+    ADT = lubridate::as_date(ADT)
+  )
+
+  expected_output <- tibble::tribble(
+    ~STUDYID,       ~USUBJID,      ~PARAMCD, ~AVALC, ~ADT,
+    "CDISCPILOT01", "01-701-1015", "OVR",    "CR",   "2016-01-25",
+    "CDISCPILOT01", "01-701-1015", "OVR",    "PD",   "2016-02-22",
+    "CDISCPILOT01", "01-701-1034", "OVR",    "SD",   "2015-12-07",
+    "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-04-25",
+    "CDISCPILOT01", "01-701-1035", "OVR",    "SD",   "2016-04-25",
+    "CDISCPILOT01", "01-701-1035", "OVR",    "PR",   "2016-06-25",
+  ) %>% dplyr::mutate(
+    ADT = lubridate::as_date(ADT)
+  )
+
+  actual_output <- filter_pd(
+    dataset = adrs,
+    filter = PARAMCD == "OVR",
+    source_pd = date_source(
+      dataset_name = "adevent",
+      date = ADT,
+      filter = PARAMCD == "PD",
+    ),
+    source_datasets = list(adevent = adevent)
+  )
+
+  expect_dfs_equal(
+    expected_output,
+    actual_output,
+    keys = c("STUDYID", "USUBJID", "ADT")
+  )
+})
+
+## filter_pd Test 2: first PD in input dataset ----
+test_that("filter_pd Test 2: first PD in input dataset", {
+  adrs <- tibble::tribble(
+    ~STUDYID,       ~USUBJID,      ~PARAMCD, ~AVALC, ~ADT,
+    "CDISCPILOT01", "01-701-1015", "OVR",    "CR",   "2016-01-25",
+    "CDISCPILOT01", "01-701-1015", "OVR",    "PD",   "2016-02-22",
+    "CDISCPILOT01", "01-701-1015", "BOR",    "CR",   "2016-01-25",
+    "CDISCPILOT01", "01-701-1034", "OVR",    "SD",   "2015-12-07",
+    "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-04-25",
+    "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-06-25",
+    "CDISCPILOT01", "01-701-1034", "BOR",    "SD",   "2015-12-07",
+    "CDISCPILOT01", "01-701-1035", "OVR",    "SD",   "2016-04-25",
+    "CDISCPILOT01", "01-701-1035", "OVR",    "PR",   "2016-06-25",
+    "CDISCPILOT01", "01-701-1035", "BOR",    "PR",   "2016-06-25",
+    "CDISCPILOT01", "01-701-1015", "PD",     "Y",    "2016-02-22",
+    "CDISCPILOT01", "01-701-1034", "PD",     "Y",    "2016-04-25"
+  ) %>% dplyr::mutate(
+    ADT = lubridate::as_date(ADT)
+  )
+
+  expected_output <- tibble::tribble(
+    ~STUDYID,       ~USUBJID,      ~PARAMCD, ~AVALC, ~ADT,
+    "CDISCPILOT01", "01-701-1015", "OVR",    "CR",   "2016-01-25",
+    "CDISCPILOT01", "01-701-1015", "OVR",    "PD",   "2016-02-22",
+    "CDISCPILOT01", "01-701-1034", "OVR",    "SD",   "2015-12-07",
+    "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-04-25",
+    "CDISCPILOT01", "01-701-1035", "OVR",    "SD",   "2016-04-25",
+    "CDISCPILOT01", "01-701-1035", "OVR",    "PR",   "2016-06-25",
+  ) %>% dplyr::mutate(
+    ADT = lubridate::as_date(ADT)
+  )
+
+  actual_output <- filter_pd(
+    dataset = adrs,
+    filter = PARAMCD == "OVR",
+    source_pd = date_source(
+      dataset_name = "adrs",
+      date = ADT,
+      filter = PARAMCD == "PD",
+    ),
+    source_datasets = list(adrs = adrs)
+  )
+
+  expect_dfs_equal(
+    expected_output,
+    actual_output,
+    keys = c("STUDYID", "USUBJID", "ADT")
+  )
+})
+
+## filter_pd Test 3: first PD derived from input dataset ----
+test_that("filter_pd Test 1: first PD derived from input dataset", {
+  adrs <- tibble::tribble(
+    ~STUDYID,       ~USUBJID,      ~PARAMCD, ~AVALC, ~ADT,
+    "CDISCPILOT01", "01-701-1015", "OVR",    "CR",   "2016-01-25",
+    "CDISCPILOT01", "01-701-1015", "OVR",    "PD",   "2016-02-22",
+    "CDISCPILOT01", "01-701-1015", "BOR",    "CR",   "2016-01-25",
+    "CDISCPILOT01", "01-701-1034", "OVR",    "SD",   "2015-12-07",
+    "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-04-25",
+    "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-06-25",
+    "CDISCPILOT01", "01-701-1034", "BOR",    "SD",   "2015-12-07",
+    "CDISCPILOT01", "01-701-1035", "OVR",    "SD",   "2016-04-25",
+    "CDISCPILOT01", "01-701-1035", "OVR",    "PR",   "2016-06-25",
+    "CDISCPILOT01", "01-701-1035", "BOR",    "PR",   "2016-06-25"
+  ) %>% dplyr::mutate(
+    ADT = lubridate::as_date(ADT)
+  )
+
+  expected_output <- tibble::tribble(
+    ~STUDYID,       ~USUBJID,      ~PARAMCD, ~AVALC, ~ADT,
+    "CDISCPILOT01", "01-701-1015", "OVR",    "CR",   "2016-01-25",
+    "CDISCPILOT01", "01-701-1015", "OVR",    "PD",   "2016-02-22",
+    "CDISCPILOT01", "01-701-1034", "OVR",    "SD",   "2015-12-07",
+    "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-04-25",
+    "CDISCPILOT01", "01-701-1035", "OVR",    "SD",   "2016-04-25",
+    "CDISCPILOT01", "01-701-1035", "OVR",    "PR",   "2016-06-25",
+  ) %>% dplyr::mutate(
+    ADT = lubridate::as_date(ADT)
+  )
+
+  actual_output <- filter_pd(
+    dataset = adrs,
+    filter = PARAMCD == "OVR",
+    source_pd = date_source(
+      dataset_name = "adrs",
+      date = ADT,
+      filter = PARAMCD == "OVR" & AVALC == "PD",
+    ),
+    source_datasets = list(adrs = adrs)
+  )
+
+  expect_dfs_equal(
+    expected_output,
+    actual_output,
+    keys = c("STUDYID", "USUBJID", "ADT")
+  )
+})
