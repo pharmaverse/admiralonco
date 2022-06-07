24b22aef65ff3ff9c9f96015d3d18f05fc637990
Bundfuss, Stefan {MDBB~Basel} <stefan.bundfuss@roche.com>
2022-06-07 12:48:14 +0200

    18_filter_pd: add ANL01FL to examples and improve documentation

--- R/filter_pd.R
+++ R/filter_pd.R
@@ -1,8 +1,9 @@
-#' Filter up to First PD Date
+#' Filter up to First PD (Progressive Disease) Date
 #'
 #' Filter a dataset to only include the source parameter records up to and
-#' including the first PD. These records are passed to downstream derivations
-#' regarding responses such as BOR.
+#' including the first PD (progressive disease). These records are passed to
+#' downstream derivations regarding responses such as BOR (best overall
+#' response).
 #'
 #' @param dataset Input dataset
 #'
@@ -39,8 +40,8 @@
 #' `source_pd$filter`.
 #'
 #' 1. The restricted input dataset is restricted to records up to first PD date.
-#' Records at first PD date are included. For subject without first PD date all
-#' records are included.
+#' Records matching first PD date are included. For subject without any first PD
+#' date, all records are included.
 #'
 #' @author Teckla Akinyi, Stefan Bundfuss
 #'
@@ -57,17 +58,18 @@
 #'
 #' # Filter OVR records up to first PD, first PD date provided in separate BDS dataset (adevent)
 #' adrs <- tibble::tribble(
-#'   ~STUDYID,       ~USUBJID,      ~PARAMCD, ~AVALC, ~ADT,
-#'   "CDISCPILOT01", "01-701-1015", "OVR",    "CR",   "2016-01-25",
-#'   "CDISCPILOT01", "01-701-1015", "OVR",    "PD",   "2016-02-22",
-#'   "CDISCPILOT01", "01-701-1015", "BOR",    "CR",   "2016-01-25",
-#'   "CDISCPILOT01", "01-701-1034", "OVR",    "SD",   "2015-12-07",
-#'   "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-04-25",
-#'   "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-06-25",
-#'   "CDISCPILOT01", "01-701-1034", "BOR",    "SD",   "2015-12-07",
-#'   "CDISCPILOT01", "01-701-1035", "OVR",    "SD",   "2016-04-25",
-#'   "CDISCPILOT01", "01-701-1035", "OVR",    "PR",   "2016-06-25",
-#'   "CDISCPILOT01", "01-701-1035", "BOR",    "PR",   "2016-06-25"
+#'   ~STUDYID,       ~USUBJID,      ~PARAMCD, ~AVALC, ~ADT,         ~ANL01FL,
+#'   "CDISCPILOT01", "01-701-1015", "OVR",    "CR",   "2016-01-25", "Y",
+#'   "CDISCPILOT01", "01-701-1015", "OVR",    "SD",   "2016-02-22", NA_character_,
+#'   "CDISCPILOT01", "01-701-1015", "OVR",    "PD",   "2016-02-22", "Y",
+#'   "CDISCPILOT01", "01-701-1015", "BOR",    "CR",   "2016-01-25", "Y",
+#'   "CDISCPILOT01", "01-701-1034", "OVR",    "SD",   "2015-12-07", "Y",
+#'   "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-04-25", "Y",
+#'   "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-06-25", "Y",
+#'   "CDISCPILOT01", "01-701-1034", "BOR",    "SD",   "2015-12-07", "Y",
+#'   "CDISCPILOT01", "01-701-1035", "OVR",    "SD",   "2016-04-25", "Y",
+#'   "CDISCPILOT01", "01-701-1035", "OVR",    "PR",   "2016-06-25", "Y",
+#'   "CDISCPILOT01", "01-701-1035", "BOR",    "PR",   "2016-06-25", "Y"
 #' ) %>% mutate(
 #'   ADT = as_date(ADT)
 #' )
@@ -80,34 +82,20 @@
 #'   ADT = as_date(ADT)
 #' )
 #'
+#' pd <- date_source(
+#'   dataset_name = "adevent",
+#'   date = ADT,
+#'   filter = PARAMCD == "PD"
+#' )
+#'
 #' filter_pd(
 #'   dataset = adrs,
-#'   filter = PARAMCD == "OVR",
-#'   source_pd = date_source(
-#'     dataset_name = "adevent",
-#'     date = ADT,
-#'     filter = PARAMCD == "PD"
-#'   ),
+#'   filter = PARAMCD == "OVR" & ANL01FL == "Y",
+#'   source_pd = pd,
 #'   source_datasets = list(adevent = adevent)
 #' )
 #'
-#' # Filter OVR records up to first PD, first PD date provided in ADSL dataset (adevent)
-#' adrs <- tibble::tribble(
-#'   ~STUDYID,       ~USUBJID,      ~PARAMCD, ~AVALC, ~ADT,
-#'   "CDISCPILOT01", "01-701-1015", "OVR",    "CR",   "2016-01-25",
-#'   "CDISCPILOT01", "01-701-1015", "OVR",    "PD",   "2016-02-22",
-#'   "CDISCPILOT01", "01-701-1015", "BOR",    "CR",   "2016-01-25",
-#'   "CDISCPILOT01", "01-701-1034", "OVR",    "SD",   "2015-12-07",
-#'   "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-04-25",
-#'   "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-06-25",
-#'   "CDISCPILOT01", "01-701-1034", "BOR",    "SD",   "2015-12-07",
-#'   "CDISCPILOT01", "01-701-1035", "OVR",    "SD",   "2016-04-25",
-#'   "CDISCPILOT01", "01-701-1035", "OVR",    "PR",   "2016-06-25",
-#'   "CDISCPILOT01", "01-701-1035", "BOR",    "PR",   "2016-06-25"
-#' ) %>% mutate(
-#'   ADT = as_date(ADT)
-#' )
-#'
+#' # Filter OVR records up to first PD, first PD date provided in ADSL dataset
 #' adsl <- tibble::tribble(
 #'   ~STUDYID,       ~USUBJID,      ~PDDT,
 #'   "CDISCPILOT01", "01-701-1015", "2016-02-22",
@@ -117,71 +105,79 @@
 #'   PDDT = as_date(PDDT)
 #' )
 #'
+#' pd <- date_source(
+#'   dataset_name = "adsl",
+#'   date = PDDT
+#' )
+#'
 #' filter_pd(
 #'   dataset = adrs,
-#'   filter = PARAMCD == "OVR",
-#'   source_pd = date_source(
-#'     dataset_name = "adsl",
-#'     date = PDDT
-#'   ),
+#'   filter = PARAMCD == "OVR" & ANL01FL == "Y",
+#'   source_pd = pd,
 #'   source_datasets = list(adsl = adsl)
 #' )
 #'
 #' # Filter OVR records up to first PD, first PD date provided in input dataset (PD parameter)
 #' adrs <- tibble::tribble(
-#'   ~STUDYID,       ~USUBJID,      ~PARAMCD, ~AVALC, ~ADT,
-#'   "CDISCPILOT01", "01-701-1015", "OVR",    "CR",   "2016-01-25",
-#'   "CDISCPILOT01", "01-701-1015", "OVR",    "PD",   "2016-02-22",
-#'   "CDISCPILOT01", "01-701-1015", "BOR",    "CR",   "2016-01-25",
-#'   "CDISCPILOT01", "01-701-1034", "OVR",    "SD",   "2015-12-07",
-#'   "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-04-25",
-#'   "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-06-25",
-#'   "CDISCPILOT01", "01-701-1034", "BOR",    "SD",   "2015-12-07",
-#'   "CDISCPILOT01", "01-701-1035", "OVR",    "SD",   "2016-04-25",
-#'   "CDISCPILOT01", "01-701-1035", "OVR",    "PR",   "2016-06-25",
-#'   "CDISCPILOT01", "01-701-1035", "BOR",    "PR",   "2016-06-25",
-#'   "CDISCPILOT01", "01-701-1015", "PD",     "Y",    "2016-02-22",
-#'   "CDISCPILOT01", "01-701-1034", "PD",     "Y",    "2016-04-25"
+#'   ~STUDYID,       ~USUBJID,      ~PARAMCD, ~AVALC, ~ADT,         ~ANL01FL,
+#'   "CDISCPILOT01", "01-701-1015", "OVR",    "CR",   "2016-01-25", "Y",
+#'   "CDISCPILOT01", "01-701-1015", "OVR",    "SD",   "2016-02-22", NA_character_,
+#'   "CDISCPILOT01", "01-701-1015", "OVR",    "PD",   "2016-02-22", "Y",
+#'   "CDISCPILOT01", "01-701-1015", "BOR",    "CR",   "2016-01-25", "Y",
+#'   "CDISCPILOT01", "01-701-1034", "OVR",    "SD",   "2015-12-07", "Y",
+#'   "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-04-25", "Y",
+#'   "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-06-25", "Y",
+#'   "CDISCPILOT01", "01-701-1034", "BOR",    "SD",   "2015-12-07", "Y",
+#'   "CDISCPILOT01", "01-701-1035", "OVR",    "SD",   "2016-04-25", "Y",
+#'   "CDISCPILOT01", "01-701-1035", "OVR",    "PR",   "2016-06-25", "Y",
+#'   "CDISCPILOT01", "01-701-1035", "BOR",    "PR",   "2016-06-25", "Y",
+#'   "CDISCPILOT01", "01-701-1015", "PD",     "Y",    "2016-02-22", "Y",
+#'   "CDISCPILOT01", "01-701-1034", "PD",     "Y",    "2016-04-25", "Y"
 #' ) %>% mutate(
 #'   ADT = as_date(ADT)
 #' )
 #'
+#' pd <- date_source(
+#'   dataset_name = "adrs",
+#'   date = ADT,
+#'   filter = PARAMCD == "PD"
+#' )
+#'
 #' filter_pd(
 #'   dataset = adrs,
-#'   filter = PARAMCD == "OVR",
-#'   source_pd = date_source(
-#'     dataset_name = "adrs",
-#'     date = ADT,
-#'     filter = PARAMCD == "PD"
-#'   ),
+#'   filter = PARAMCD == "OVR" & ANL01FL == "Y",
+#'   source_pd = pd,
 #'   source_datasets = list(adrs = adrs)
 #' )
 #'
 #' # Filter OVR records up to first PD, first PD date derived from OVR records
 #' adrs <- tibble::tribble(
-#'   ~STUDYID,       ~USUBJID,      ~PARAMCD, ~AVALC, ~ADT,
-#'   "CDISCPILOT01", "01-701-1015", "OVR",    "CR",   "2016-01-25",
-#'   "CDISCPILOT01", "01-701-1015", "OVR",    "PD",   "2016-02-22",
-#'   "CDISCPILOT01", "01-701-1015", "BOR",    "CR",   "2016-01-25",
-#'   "CDISCPILOT01", "01-701-1034", "OVR",    "SD",   "2015-12-07",
-#'   "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-04-25",
-#'   "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-06-25",
-#'   "CDISCPILOT01", "01-701-1034", "BOR",    "SD",   "2015-12-07",
-#'   "CDISCPILOT01", "01-701-1035", "OVR",    "SD",   "2016-04-25",
-#'   "CDISCPILOT01", "01-701-1035", "OVR",    "PR",   "2016-06-25",
-#'   "CDISCPILOT01", "01-701-1035", "BOR",    "PR",   "2016-06-25"
+#'   ~STUDYID,       ~USUBJID,      ~PARAMCD, ~AVALC, ~ADT,         ~ANL01FL,
+#'   "CDISCPILOT01", "01-701-1015", "OVR",    "CR",   "2016-01-25", "Y",
+#'   "CDISCPILOT01", "01-701-1015", "OVR",    "SD",   "2016-02-22", NA_character_,
+#'   "CDISCPILOT01", "01-701-1015", "OVR",    "PD",   "2016-02-22", "Y",
+#'   "CDISCPILOT01", "01-701-1015", "BOR",    "CR",   "2016-01-25", "Y",
+#'   "CDISCPILOT01", "01-701-1034", "OVR",    "SD",   "2015-12-07", "Y",
+#'   "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-04-25", "Y",
+#'   "CDISCPILOT01", "01-701-1034", "OVR",    "PD",   "2016-06-25", "Y",
+#'   "CDISCPILOT01", "01-701-1034", "BOR",    "SD",   "2015-12-07", "Y",
+#'   "CDISCPILOT01", "01-701-1035", "OVR",    "SD",   "2016-04-25", "Y",
+#'   "CDISCPILOT01", "01-701-1035", "OVR",    "PR",   "2016-06-25", "Y",
+#'   "CDISCPILOT01", "01-701-1035", "BOR",    "PR",   "2016-06-25", "Y"
 #' ) %>% mutate(
 #'   ADT = as_date(ADT)
 #' )
 #'
+#' pd <- date_source(
+#'   dataset_name = "adrs",
+#'   date = ADT,
+#'   filter = PARAMCD == "OVR" & ANL01FL == "Y" & AVALC == "PD"
+#' )
+#'
 #' filter_pd(
 #'   dataset = adrs,
-#'   filter = PARAMCD == "OVR",
-#'   source_pd = date_source(
-#'     dataset_name = "adrs",
-#'     date = ADT,
-#'     filter = PARAMCD == "OVR" & AVALC == "PD"
-#'   ),
+#'   filter = PARAMCD == "OVR" & ANL01FL == "Y",
+#'   source_pd = pd,
 #'   source_datasets = list(adrs = adrs)
 #' )
 filter_pd <- function(dataset,
