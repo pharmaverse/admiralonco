579fdfd78c8eacaa3c3b069ff0f89103d079e725
amsmith214 <andrew.m.smith@gsk.com>
2022-06-08 17:02:16 +0000

    quo_c

--- R/admiralonco-package.R
+++ R/admiralonco-package.R
@@ -2,7 +2,7 @@
 #' @importFrom admiral assert_character_scalar assert_data_frame
 #'   assert_filter_cond assert_list_of assert_order_vars assert_s3_class
 #'   assert_vars derive_var_obs_number derive_vars_merged expect_dfs_equal
-#'   filter_if vars2chr date_source
+#'   filter_if vars2chr date_source quo_c
 #' @importFrom dplyr filter group_by if_else left_join mutate select slice
 #'   vars ungroup bind_rows
 #' @importFrom magrittr %>%
