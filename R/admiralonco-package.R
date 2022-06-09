45c3ffbc062b145b0343341a3e9dd98195f13a12
amsmith214 <andrew.m.smith@gsk.com>
2022-06-09 11:33:56 +0000

    imports

--- R/admiralonco-package.R
+++ R/admiralonco-package.R
@@ -1,10 +1,11 @@
 #' @keywords internal
 #' @importFrom admiral assert_character_scalar assert_data_frame
-#'   assert_filter_cond assert_list_of assert_order_vars assert_s3_class
-#'   assert_vars derive_var_obs_number derive_vars_merged expect_dfs_equal
-#'   filter_if vars2chr date_source
-#' @importFrom dplyr filter group_by if_else left_join mutate select slice
-#'   vars ungroup bind_rows
-#' @importFrom magrittr %>%
-#' @importFrom rlang abort enquo quo_is_null
+#'   assert_filter_cond assert_integer_scalar assert_list_of assert_order_vars
+#'   assert_param_does_not_exist assert_s3_class assert_symbol assert_vars assert_varval_list
+#'   date_source derive_var_obs_number derive_vars_merged enumerate expect_dfs_equal
+#'   extract_vars filter_extreme filter_if filter_relative quo_c squote vars2chr 
+#' @importFrom dplyr anti_join bind_rows filter group_by if_else left_join mutate
+#'   right_join select slice vars ungroup
+#' @importFrom magrittr `%>%`
+#' @importFrom rlang abort enquo quo_get_expr quo_is_null 
 "_PACKAGE"
