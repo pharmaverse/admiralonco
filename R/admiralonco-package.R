5a1874bec7eebfea91b17da80638d0c3817942ea
amsmith214 <andrew.m.smith@gsk.com>
2022-06-09 12:33:23 +0000

    style/lint for package file

--- R/admiralonco-package.R
+++ R/admiralonco-package.R
@@ -3,10 +3,10 @@
 #'   assert_filter_cond assert_integer_scalar assert_list_of assert_order_vars
 #'   assert_param_does_not_exist assert_s3_class assert_symbol assert_vars assert_varval_list
 #'   date_source derive_var_obs_number derive_vars_merged enumerate expect_dfs_equal
-#'   extract_vars filter_extreme filter_if filter_relative quo_c squote vars2chr 
+#'   extract_vars filter_extreme filter_if filter_relative quo_c squote vars2chr
 #' @importFrom dplyr anti_join bind_rows filter group_by if_else left_join mutate
 #'   rename right_join select slice vars ungroup
 #' @importFrom lubridate days
 #' @importFrom magrittr %>%
-#' @importFrom rlang abort enquo quo_get_expr quo_is_null 
+#' @importFrom rlang abort enquo quo_get_expr quo_is_null
 "_PACKAGE"
