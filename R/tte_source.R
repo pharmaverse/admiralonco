7a91b1eef5e468bb72ee7582a7e46417c9f9f790
Matt Marino <matthew.j.marino@gsk.com>
2022-06-17 17:40:12 -0400

    #31 updated documentation to have more information

--- R/tte_source.R
+++ R/tte_source.R
@@ -4,16 +4,31 @@
 #'
 #' @details
 #' To see the definition of the various objects simply print the object in the
-#' R console, e.g. `print(death_event)`.
+#' R console, e.g. `print(death_event)`. For details of how to use these objects
+#' please refer to [admiral::derive_param_tte()].
+#'
+#' Printing an object will display input dataset_name, filter (if applicable), date variable,
+#' and appropriate values for EVNTDESC, CNSDTDSC, SRCDOM, SRCVAR, and SRCSEQ.
 #'
 #' @seealso [admiral::derive_param_tte], [tte_source], [admiral::event_source],
 #'  [admiral::censor_source]
+#'
+#' @format NULL
 #'
 #' @export
 #'
 #' @keywords tte_source
 #'
 #' @rdname tte_source_objects
+#'
+#' @examples
+#' # This shows the definition of all pre-defined `tte_source` objects that ship
+#' # with {admiralonco}
+#' for (obj in list_tte_source_objects()$object) {
+#'   cat(obj, "\n")
+#'   print(get(obj))
+#'   cat("\n")
+#' }
 #'
 death_event <- event_source(
   dataset_name = "adrs",
