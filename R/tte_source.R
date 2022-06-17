751d56e9791f4ef0e3ef54e6f3b4440d45fb0e3e
mattmarino <102807636+mattmarino@users.noreply.github.com>
2022-06-16 22:57:16 -0400

    Update tte_source.R
    #31 - changes accidentally made in onco_tte_sources - copied to here

--- R/tte_source.R
+++ R/tte_source.R
@@ -1,12 +1,13 @@
 #' Pre-Defined Time-to-Event Source Objects
 #'
-#' These pre-defined `tte_source` objects can be used as input to `derive_param_tte()`.
+#' These pre-defined `tte_source` objects can be used as input to 'admiral::derive_param_tte()`.
 #'
 #' @details
 #' To see the definition of the various objects simply print the object in the
 #' R console, e.g. `print(death_event)`.
 #'
-#' @seealso [derive_param_tte], [tte_source], [event_source], [censor_source]
+#' @seealso [admiral::derive_param_tte], [tte_source], [admiral::event_source],
+#'  [admiral::censor_source]
 #'
 #' @export
 #'
@@ -19,8 +20,8 @@ death_event <- event_source(
   filter = PARAMCD == "DEATH" & AVALC == "Y" & ANL01FL == "Y",
   date = ADT,
   set_values_to = vars(
-    EVNTDESC = "DEATH",
-    CNSDTDSC = "",
+    EVNTDESC = "Death",
+    CNSDTDSC = "NA_character_",
     SRCDOM = "ADRS",
     SRCVAR = "ADT",
     SRCSEQ = ASEQ
@@ -34,11 +35,11 @@ lastalive_censor <- censor_source(
   dataset_name = "adsl",
   date = LSTALVDT,
   set_values_to = vars(
-    EVNTDESC = "ALIVE",
+    EVNTDESC = "Alive",
     CNSDTDSC = "Alive During Study",
     SRCDOM = "ADSL",
     SRCVAR = "LSTALVDT",
-    SRCSEQ =
+    SRCSEQ = NA_integer_
   )
 )
 
@@ -51,7 +52,7 @@ pd_event <- event_source(
   date = ADT,
   set_values_to = vars(
     EVNTDESC = "Disease Progression",
-    CNSDTDSC = ,
+    CNSDTDSC = "NA_character_",
     SRCDOM = "ADRS",
     SRCVAR = "ADT",
     SRCSEQ = ASEQ
@@ -66,7 +67,7 @@ lasta_censor <- censor_source(
   filter = PARAMCD == "LSTA" & ANL01FL == "Y",
   date = ADT,
   set_values_to = vars(
-    EVNTDESC = "Last TUmor Assessment",
+    EVNTDESC = "Last Tumor Assessment",
     CNSDTDSC = "Last Tumor Assessment",
     SRCDOM = "ADRS",
     SRCVAR = "ADT",
@@ -85,7 +86,7 @@ rand_censor <- censor_source(
     CNSDTDSC = "Randomization",
     SRCDOM = "ADSL",
     SRCVAR = "RANDDT",
-    SRCSEQ =
+    SRCSEQ = NA_integer_
   )
 )
 
@@ -100,157 +101,6 @@ trts_censor <- censor_source(
     CNSDTDSC = "Treatment Start",
     SRCDOM = "ADSL",
     SRCVAR = "TRTSDT",
-    SRCSEQ =
-  )
-)
-
-
-#' @keywords tte_source
-#' @rdname tte_source_objects
-#' @export
-ae_event <- event_source(
-  dataset_name = "adae",
-  filter = TRTEMFL == "Y",
-  date = ASTDT,
-  set_values_to = vars(
-    EVNTDESC = "ADVERSE EVENT",
-    SRCDOM = "ADAE",
-    SRCVAR = "ASTDT",
-    SRCSEQ = AESEQ
-  )
-)
-
-#' @keywords tte_source
-#' @rdname tte_source_objects
-#' @export
-ae_ser_event <- event_source(
-  dataset_name = "adae",
-  filter = TRTEMFL == "Y" & AESER == "Y",
-  date = ASTDT,
-  set_values_to = vars(
-    EVNTDESC = "SERIOUS ADVERSE EVENT",
-    SRCDOM = "ADAE",
-    SRCVAR = "ASTDT",
-    SRCSEQ = AESEQ
-  )
-)
-
-#' @keywords tte_source
-#' @rdname tte_source_objects
-#' @export
-ae_gr1_event <- event_source(
-  dataset_name = "adae",
-  filter = TRTEMFL == "Y" & ATOXGR == "1",
-  date = ASTDT,
-  set_values_to = vars(
-    EVNTDESC = "GRADE 1 ADVERSE EVENT",
-    SRCDOM = "ADAE",
-    SRCVAR = "ASTDT",
-    SRCSEQ = AESEQ
-  )
-)
-
-#' @keywords tte_source
-#' @rdname tte_source_objects
-#' @export
-ae_gr2_event <- event_source(
-  dataset_name = "adae",
-  filter = TRTEMFL == "Y" & ATOXGR == "2",
-  date = ASTDT,
-  set_values_to = vars(
-    EVNTDESC = "GRADE 2 ADVERSE EVENT",
-    SRCDOM = "ADAE",
-    SRCVAR = "ASTDT",
-    SRCSEQ = AESEQ
-  )
-)
-
-#' @keywords tte_source
-#' @rdname tte_source_objects
-#' @export
-ae_gr3_event <- event_source(
-  dataset_name = "adae",
-  filter = TRTEMFL == "Y" & ATOXGR == "3",
-  date = ASTDT,
-  set_values_to = vars(
-    EVNTDESC = "GRADE 3 ADVERSE EVENT",
-    SRCDOM = "ADAE",
-    SRCVAR = "ASTDT",
-    SRCSEQ = AESEQ
-  )
-)
-
-#' @keywords tte_source
-#' @rdname tte_source_objects
-#' @export
-ae_gr4_event <- event_source(
-  dataset_name = "adae",
-  filter = TRTEMFL == "Y" & ATOXGR == "4",
-  date = ASTDT,
-  set_values_to = vars(
-    EVNTDESC = "GRADE 4 ADVERSE EVENT",
-    SRCDOM = "ADAE",
-    SRCVAR = "ASTDT",
-    SRCSEQ = AESEQ
-  )
-)
-
-#' @keywords tte_source
-#' @rdname tte_source_objects
-#' @export
-ae_gr5_event <- event_source(
-  dataset_name = "adae",
-  filter = TRTEMFL == "Y" & ATOXGR == "5",
-  date = ASTDT,
-  set_values_to = vars(
-    EVNTDESC = "GRADE 5 ADVERSE EVENT",
-    SRCDOM = "ADAE",
-    SRCVAR = "ASTDT",
-    SRCSEQ = AESEQ
-  )
-)
-
-#' @keywords tte_source
-#' @rdname tte_source_objects
-#' @export
-ae_gr35_event <- event_source(
-  dataset_name = "adae",
-  filter = TRTEMFL == "Y" & ATOXGR %in% c("3", "4", "5"),
-  date = ASTDT,
-  set_values_to = vars(
-    EVNTDESC = "GRADE 3-5 ADVERSE EVENT",
-    SRCDOM = "ADAE",
-    SRCVAR = "ASTDT",
-    SRCSEQ = AESEQ
-  )
-)
-
-#' @keywords tte_source
-#' @rdname tte_source_objects
-#' @export
-ae_sev_event <- event_source(
-  dataset_name = "adae",
-  filter = TRTEMFL == "Y" & AESEV == "SEVERE",
-  date = ASTDT,
-  set_values_to = vars(
-    EVNTDESC = "SEVERE ADVERSE EVENT",
-    SRCDOM = "ADAE",
-    SRCVAR = "ASTDT",
-    SRCSEQ = AESEQ
-  )
-)
-
-#' @keywords tte_source
-#' @rdname tte_source_objects
-#' @export
-ae_wd_event <- event_source(
-  dataset_name = "adae",
-  filter = TRTEMFL == "Y" & AEACN == "DRUG WITHDRAWN",
-  date = ASTDT,
-  set_values_to = vars(
-    EVNTDESC = "ADVERSE EVENT LEADING TO DRUG WITHDRAWAL",
-    SRCDOM = "ADAE",
-    SRCVAR = "ASTDT",
-    SRCSEQ = AESEQ
+    SRCSEQ = NA_integer_
   )
 )
