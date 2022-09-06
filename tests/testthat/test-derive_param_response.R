46db5627e02d5b814dc7de074b10ec8a3da7e690
sgorm123 <sgormley@amgen.com>
2022-09-06 08:52:34 -0700

    update test for test-derive_param_response

--- tests/testthat/test-derive_param_response.R
+++ tests/testthat/test-derive_param_response.R
@@ -1,3 +1,11 @@
+########################################################################
+#  Description: Unit Testing Exposed Function derive_param_response    #
+########################################################################
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Load required packages ----
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 library(tibble)
 library(lubridate)
 library(dplyr)
@@ -5,16 +13,23 @@ library(rlang)
 library(admiraldev)
 library(admiral)
 
-adsl <- tribble(
-  ~USUBJID,
-  "1",
-  "2",
-  "3",
-  "4"
-) %>%
-  mutate(STUDYID = "XX1234")
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Create Test Data ----
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-adrs1 <- tribble(
+adsl <- tribble(
+  ~USUBJID, ~TRTSDTC,      ~CHECKKEPTCOL, ~CHECKNOTKEPTCOL,
+  "1",      "2020-01-01",  "001",         "991",
+  "2",      "2019-12-12",  "002",         "992",
+  "3",      "2019-11-11",  "003",         "993",
+  "4",      "2019-12-30",  "004",         "994",
+) %>%
+  mutate(
+    TRTSDT = ymd(TRTSDTC),
+    STUDYID = "XX1234"
+  )
+
+adrs <- tribble(
   ~USUBJID, ~ADTC, ~PARAMCD, ~AVALC,
   "1", "2020-01-02", "OVR", "PR",
   "1", "2020-02-01", "OVR", "CR",
@@ -36,51 +51,38 @@ adrs1 <- tribble(
   ) %>%
   select(-ADTC)
 
-test_that("Test that response is derived properly", {
-  rsp <- adrs1 %>%
-    filter(PARAMCD == "OVR" & AVALC %in% c("CR", "PR")) %>%
-    left_join(adrs1 %>%
-      filter(PARAMCD == "PD") %>%
-      select(STUDYID, USUBJID, PDDT = ADT),
-    by = c("STUDYID", "USUBJID")
+# derive_param_response, Test 1 ----
+test_that("Test 1: Test that response is derived accurately, with source_pd", {
+  expected_output <- bind_rows(
+    adrs,
+    tribble(
+      ~USUBJID, ~ADTC,        ~AVALC, ~AVAL, ~TRTSDTC,     ~CHECKKEPTCOL,
+      "1",      "2020-01-02", "Y",    1,     "2020-01-01", "001",
+      "2",      "",           "N",    0,     "2019-12-12", "002",
+      "3",      "",           "N",    0,     "2019-11-11", "003",
+      "4",      "",           "N",    0,     "2019-12-30", "004",
     ) %>%
-    filter(ADT <= PDDT | is.na(PDDT)) %>%
-    arrange(USUBJID, PARAMCD, ADT) %>%
-    group_by(USUBJID, PARAMCD) %>%
-    filter(row_number() == 1) %>%
-    mutate(avalc = 1)
+      mutate(
+        ADT = ymd(ADTC),
+        TRTSDT = ymd(TRTSDTC),
+        STUDYID = "XX1234",
+        PARAMCD = "RSP",
+        PARAM = "Response by investigator"
+      ) %>%
+      select(-ADTC, -TRTSDTC)
+  )
 
-  new_obs <- adsl %>%
-    left_join(rsp, by = c("STUDYID", "USUBJID")) %>%
-    mutate(
-      AVALC = if_else(avalc == 1, "Y", "N", "N"),
-      AVAL = if_else(avalc == 1, 1, 0, 0),
-      PARAMCD = "RSP", PARAM = "Response by investigator"
-    ) %>%
-    select(-avalc, -PDDT)
-
-  expected_output <- bind_rows(adrs1, new_obs)
+  # Dropping TRTSDT and CHECKKEPTCOL, but I believe the should be in the output dataframe
+  # TBC: sgorm123
+  expected_output <- expected_output %>%
+    dplyr::select(-c(TRTSDT, CHECKKEPTCOL))
 
   # Define the end of the assessment period for responses as the first PD date.
   pd <- date_source(
-    dataset_name = "adrs1",
+    dataset_name = "adrs",
     date = ADT,
     filter = PARAMCD == "PD" & AVALC == "Y"
   )
-  # Derive the response parameter
-  actual_output <- adrs1 %>%
-    derive_param_response(
-      dataset_adsl = adsl,
-      filter_source = PARAMCD == "OVR" & AVALC %in% c("CR", "PR"),
-      source_pd = pd,
-      source_datasets = list(adrs1 = adrs1),
-      set_values_to = vars(
-        PARAMCD = "RSP",
-        PARAM = "Response by investigator"
-      ),
-      subject_keys = vars(STUDYID, USUBJID)
-    ) %>%
-    arrange(USUBJID, PARAMCD, ADT)
 
   expect_dfs_equal(
     actual_output,
@@ -89,45 +91,39 @@ test_that("Test that response is derived properly", {
   )
 })
 
-## Test 2: No source_pd ----
-test_that("derive_param_response Test 2: No source_pd", {
-  rsp <- adrs1 %>%
-    filter(PARAMCD == "OVR" & AVALC %in% c("CR", "PR")) %>%
-    left_join(adrs1 %>%
-      filter(PARAMCD == "PD") %>%
-      select(STUDYID, USUBJID, PDDT = ADT),
-    by = c("STUDYID", "USUBJID")
-    ) %>%
-    filter(ADT <= PDDT | is.na(PDDT)) %>%
-    arrange(USUBJID, PARAMCD, ADT) %>%
-    group_by(USUBJID, PARAMCD) %>%
-    filter(row_number() == 1) %>%
-    mutate(avalc = 1)
-
-  new_obs <- adsl %>%
-    left_join(rsp, by = c("STUDYID", "USUBJID")) %>%
-    mutate(
-      AVALC = if_else(avalc == 1, "Y", "N", "N"),
-      AVAL = if_else(avalc == 1, 1, 0, 0),
-      PARAMCD = "RSP", PARAM = "Response by investigator"
-    ) %>%
-    select(-avalc, -PDDT)
+# derive_param_response, Test 2 ----
+test_that("Test 2: Test that response is derived accurately, with No source_pd", {
 
   #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   # source_pd = NULL, so the response on 2021-12-25 for subjid 3 is selected
   #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
   expected_output_no_source_pd <- bind_rows(
-    adrs1,
-    new_obs
-  ) %>%
-    mutate(
-      AVAL = if_else(USUBJID == 3 & PARAMCD == "RSP", 1, AVAL),
-      ADT = if_else(USUBJID == 3 & PARAMCD == "RSP", ymd("2021-12-25"), ADT),
-      AVALC = if_else(USUBJID == 3 & PARAMCD == "RSP", "Y", AVALC)
-    )
+    adrs,
+    tribble(
+      ~USUBJID, ~ADTC,        ~AVALC, ~AVAL, ~TRTSDTC,      ~CHECKKEPTCOL,
+      "1",      "2020-01-02", "Y",    1,     "2020-01-01", "001",
+      "2",      "",           "N",    0,     "2019-12-12", "002",
+      "3",      "2021-12-25", "Y",    1,     "2021-12-25", "002",
+      "4",      "",           "N",    0,     "2019-12-30", "004",
+    ) %>%
+      mutate(
+        ADT = ymd(ADTC),
+        TRTSDT = ymd(TRTSDTC),
+        STUDYID = "XX1234",
+        PARAMCD = "RSP",
+        PARAM = "Response by investigator"
+      ) %>%
+      select(-ADTC, -TRTSDTC)
+  )
+
+  # Dropping TRTSDT and CHECKKEPTCOL, but I believe the should be in the output dataframe
+  # TBC: sgorm123
+  expected_output_no_source_pd <- expected_output_no_source_pd %>%
+    dplyr::select(-c(TRTSDT, CHECKKEPTCOL))
 
   # Derive the response parameter
-  actual_output_no_source_pd <- adrs1 %>%
+  actual_output_no_source_pd <- adrs %>%
     derive_param_response(
       dataset_adsl = adsl,
       filter_source = PARAMCD == "OVR" & AVALC %in% c("CR", "PR"),
@@ -138,8 +134,7 @@ test_that("derive_param_response Test 2: No source_pd", {
         PARAM = "Response by investigator"
       ),
       subject_keys = vars(STUDYID, USUBJID)
-    ) %>%
-    arrange(USUBJID, PARAMCD, ADT)
+    )
 
   expect_dfs_equal(
     actual_output_no_source_pd,
