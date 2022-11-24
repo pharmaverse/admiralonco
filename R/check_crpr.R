e268219b38b185c46759bc3f8c2618656b0b5449
Stefan Bundfuss <stefan.bundfuss@roche.com>
2022-11-24 13:33:55 +0000

    191_remove_set_dataset: use admiralonco_environment instead of set_dataset()

--- R/check_crpr.R
+++ R/check_crpr.R
@@ -80,7 +80,7 @@ signal_crpr <- function(dataset,
     crpr_data <- bind_rows(crpr_data, pr_data) %>%
       arrange(!!!subject_keys, !!!order)
 
-    set_dataset(crpr_data, "crpr")
+    admiralonco_environment$crpr = crpr_data
     full_msg <- paste0(
       msg,
       "\nRun `get_crpr_dataset()` to access the CR records records followed by PR"
@@ -140,5 +140,5 @@ signal_crpr <- function(dataset,
 #'
 #' get_crpr_dataset()
 get_crpr_dataset <- function() {
-  get_dataset("crpr")
+  admiralonco_environment$crpr
 }
