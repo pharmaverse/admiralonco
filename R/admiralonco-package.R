5b38c9146f629551bda5851436e947b7f9de5d74
Stefan Bundfuss <stefan.bundfuss@roche.com>
2023-10-12 09:17:17 +0000

    #258 fix_importfrom: remove assert_order_vars

--- R/admiralonco-package.R
+++ R/admiralonco-package.R
@@ -8,9 +8,9 @@
 #' @importFrom admiraldev assert_character_scalar assert_character_vector
 #'   assert_data_frame assert_filter_cond assert_list_of assert_function
 #'   assert_integer_scalar assert_list_of assert_logical_scalar
-#'   assert_order_vars assert_param_does_not_exist assert_s3_class assert_symbol
-#'   assert_vars assert_varval_list enumerate expect_dfs_equal extract_vars
-#'   filter_if expr_c process_set_values_to squote suppress_warning vars2chr
+#'   assert_param_does_not_exist assert_s3_class assert_symbol assert_vars
+#'   assert_varval_list enumerate expect_dfs_equal extract_vars filter_if expr_c
+#'   process_set_values_to squote suppress_warning vars2chr
 #' @importFrom dplyr arrange bind_rows case_when filter group_by if_else
 #'   left_join mutate rename right_join select slice ungroup
 #' @importFrom lifecycle deprecate_warn deprecated deprecate_stop
